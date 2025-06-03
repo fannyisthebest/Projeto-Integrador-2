84f9a1157341a152a3bd1ecfbbff7479359dadd7
Kamilly <fanny.lol.kah@gmail.com>
2025-06-03 17:15:35 -0300

    botao de excluir contato

--- site-imobiliaria/js/contato-lista.js
+++ site-imobiliaria/js/contato-lista.js
@@ -3,21 +3,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let contatos = JSON.parse(localStorage.getItem('contatos')) || [];
 
-  if (contatos.length === 0) {
-    lista.innerHTML = '<p>Nenhum contato cadastrado.</p>';
-  } else {
-    contatos.forEach(function(contato, index) {
-      const div = document.createElement('div');
-      div.classList.add('contato-item');
-      div.innerHTML = `
-        <p><strong>Nome:</strong> ${contato.nome}</p>
-        <p><strong>Telefone:</strong> ${contato.telefone}</p>
-        <p><strong>E-mail:</strong> ${contato.email}</p>
-        <p><strong>Pretensão:</strong> ${contato.pretensao || 'Não informado'}</p>
-        <p><strong>Mensagem:</strong> ${contato.mensagem}</p>
-        <hr/>
-      `;
-      lista.appendChild(div);
-    });
+  function renderContatos() {
+    lista.innerHTML = '';
+
+    if (contatos.length === 0) {
+      lista.innerHTML = '<p>Nenhum contato cadastrado.</p>';
+    } else {
+      contatos.forEach(function(contato, index) {
+        const div = document.createElement('div');
+        div.classList.add('contato-item');
+
+        div.innerHTML = `
+          <p><strong>Nome:</strong> ${contato.nome}</p>
+          <p><strong>Telefone:</strong> ${contato.telefone}</p>
+          <p><strong>E-mail:</strong> ${contato.email}</p>
+          <p><strong>Pretensão:</strong> ${contato.pretensao || 'Não informado'}</p>
+          <p><strong>Mensagem:</strong> ${contato.mensagem}</p>
+          <button class="btn-excluir">Excluir</button>
+          <hr/>
+        `;
+
+        const btnExcluir = div.querySelector('.btn-excluir');
+        btnExcluir.addEventListener('click', function() {
+          contatos.splice(index, 1);
+          localStorage.setItem('contatos', JSON.stringify(contatos));
+          renderContatos();
+        });
+
+        lista.appendChild(div);
+      });
+    }
   }
+
+  renderContatos();
 });
